eea6c47cf429e13e683f039cf8a83b5b0ab225d7
Sami E Heikkinen <sami.e.heikkinen@terveystalo.com>
2024-09-11 22:22:26 +0300

    remove log

--- src/Level.ts
+++ src/Level.ts
@@ -413,7 +413,6 @@ export class Level implements Area {
                     cx.textBaseline = "middle";
                     cx.fillStyle = "rgba(255, 255, 255, 0.1)";
 
-                    console.info(element.width);
                     const spacing =
                         element.width / (element.width > 70 ? 9 : 7);
 
